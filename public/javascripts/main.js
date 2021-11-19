63123a7215ca9e30dc91b2a8286792ac90a9db5e
Colin Poon <me@colinpoon.com>
2021-11-18 18:08:25 -0800

    added vector grphic file

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -5,11 +5,11 @@ document.addEventListener('click', function(event) {
     toggle = !toggle;
 
     if (target.className === "test" && toggle) {
-        gsap.to("h1", {duration: 2, opacity: 0.3, backgroundColor: "chocolate", rotation: 360});
+        gsap.to("h1", {duration: 2, opacity: 0.3, color:"white", backgroundColor: "blue", rotation: 360, ease: "bounce"});
     }
 
     if (target.className === "test" && !toggle) {
-        gsap.to("h1", {duration: 2, opacity: 1.0, backgroundColor: "white", rotation: 0});
+        gsap.to("h1", {duration: 2, opacity: 1.0, color:"black", backgroundColor: "white", rotation: 0, ease: "bounce"});
     }
 });
 
