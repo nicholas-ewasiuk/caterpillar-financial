a1561e334ae629fdc4032a64dcb009f8ed5ebab9
Colin Poon <me@colinpoon.com>
2021-11-22 18:18:08 -0800

    graph id event firing onclick

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,10 +1,10 @@
-// let toggle = false;
-// let meltToggle = false;
+let toggle = false;
+let resize = false;
 
-// gsap.set('#level', { 
-//     scaleY: 0,
-//     transformOrigin: '50% 100%' 
-// });
+gsap.set('#level', { 
+    scaleY: 0,
+    transformOrigin: '50% 100%' 
+});
 
 // document.addEventListener('click', function(event) {
 //     const target = event.target;
@@ -19,29 +19,30 @@
 //     }
 // });
 
-// document.addEventListener('click', function(event) {
-//     const target = event.target.closest('svg');
-//     meltToggle = !meltToggle;
 
-//     if (target.id === 'meltingpot' && meltToggle) {
-//         gsap.to('#level', {duration: 3, scaleY: 1, ease: 'elastic(1, 0.8)'});
-//         console.log('innnermeltclick');
-//     }
+document.addEventListener('click', function(event) {
+    const target = event.target.closest('svg');
+    resize = !resize;
 
-//     if (target.id === 'meltingpot' && !meltToggle) {
-//         gsap.to('#level', {duration: 3, scaleY: 0, ease: 'power1.in'});
-//     }
-//     console.log(target);
-// });
+    if (target.id === 'second-col' && resize) {
+        gsap.to('#level', {duration: 3, scaleY: 1, ease: 'elastic(1, 0.8)'});
+        console.log('innnermeltclick');
+    }
+
+    if (target.id === 'second-col' && !resize) {
+        gsap.to('#level', {duration: 3, scaleY: 0, ease: 'power1.in'});
+    }
+    console.log(target);
+});
 
 // *****************************************************************************************
 // TEST SCRIPT FOR TOGGLE
 
-$(document).ready(function() {
-	$('[data-toggle="toggle"]').change(function(){
-		$(this).parents().next('.hide').toggle();
-	});
-});
+// $(document).ready(function() {
+// 	$('[data-toggle="toggle"]').change(function(){
+// 		$(this).parents().next('.hide').toggle();
+// 	});
+// });
 
 
 
