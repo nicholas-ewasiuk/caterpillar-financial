d03db5fecc50c03a25c500c5c9b42c7e4ed735d0
Colin Poon <me@colinpoon.com>
2021-11-22 16:57:00 -0800

    replaced svg grid

--- public/javascripts/keyboard-input.js
+++ public/javascripts/keyboard-input.js
@@ -1,63 +1,63 @@
-document.addEventListener('mousedown');
-document.addEventListener('mousemove')
-document.addEventListener('mouseup')
+// document.addEventListener('mousedown');
+// document.addEventListener('mousemove')
+// document.addEventListener('mouseup')
 
 
 
 
-let rightPressed = false;
-let leftPressed = false;
-let upPressed = false;
-let downPressed = false;
+// let rightPressed = false;
+// let leftPressed = false;
+// let upPressed = false;
+// let downPressed = false;
 
-function keyDownHandler(event) {
-  if(event.keyCode == 39) {
-    rightPressed = true;
-  }
-  else if(event.keyCode == 37) {
-    leftPressed = true;
-  }
-  if(event.keyCode == 40) {
-    downPressed = true;
-  }
-  else if(event.keyCode == 38) {
-    upPressed = true;
-  }
-}
+// function keyDownHandler(event) {
+//   if(event.keyCode == 39) {
+//     rightPressed = true;
+//   }
+//   else if(event.keyCode == 37) {
+//     leftPressed = true;
+//   }
+//   if(event.keyCode == 40) {
+//     downPressed = true;
+//   }
+//   else if(event.keyCode == 38) {
+//     upPressed = true;
+//   }
+// }
 
-function keyUpHandler(event) {
-  if(event.keyCode == 39) {
-    rightPressed = false;
-  }
-  else if(event.keyCode == 37) {
-    leftPressed = false;
-  }
-  if(event.keyCode == 40) {
-    downPressed = false;
-  }
-  else if(event.keyCode == 38) {
-    upPressed = false;
-  }
-}
+// function keyUpHandler(event) {
+//   if(event.keyCode == 39) {
+//     rightPressed = false;
+//   }
+//   else if(event.keyCode == 37) {
+//     leftPressed = false;
+//   }
+//   if(event.keyCode == 40) {
+//     downPressed = false;
+//   }
+//   else if(event.keyCode == 38) {
+//     upPressed = false;
+//   }
+// }
 
-function draw() {
-  if(rightPressed) {
-    rect2.transform = `translate(${rect2.x + 1}``;
-    console.log('right');
-  }
-  else if(leftPressed) {
-    rect2.x -= 1;
-    console.log('left');
-  }
-  if(downPressed) {
-    rect2.y += 1;
-    console.log('down');
-  }
-  else if(upPressed) {
-    rect2.y -= 1;
-    console.log('up');
-  }
-  window.requestAnimationFrame(draw);
-}
+// function draw() {
+//   if(rightPressed) {
+//     rect2.transform = `translate(${rect2.x + 1}`;
+//     console.log('right');
+//   }
+//   else if(leftPressed) {
+//     rect2.x -= 1;
+//     console.log('left');
+//   }
+//   if(downPressed) {
+//     rect2.y += 1;
+//     console.log('down');
+//   }
+//   else if(upPressed) {
+//     rect2.y -= 1;
+//     console.log('up');
+//   }
+//   window.requestAnimationFrame(draw);
+// }
 
-draw();
+// draw();
