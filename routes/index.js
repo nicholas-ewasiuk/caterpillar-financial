efc2241be27c3f102787e01b49852282896cdae5
panoramine <camila.c.a.salles@gmail.com>
2021-11-22 18:39:00 -0800

    add login and users routes

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,28 @@ module.exports = (db) => {
     res.render('index', { title: 'Express' });
   });
 
+  router.get('/login', function(req, res) {
+    res.render('login');
+  });
+
+  router.post('/login', function(req, res) {
+    db.query(`SELECT * FROM users WHERE email LIKE '${req.body.email}'`)
+      .then(data => {
+        const user_id = data.rows[0].id;
+
+        res.redirect('/users/' + encodeURIComponent(user_id));
+      })
+  });
+
+  router.get('/users/:id', function(req, res) {
+    const user_id = req.params.id;
+
+    
+
+
+    res.render('user');
+  });
+
   return router;
 }
 
--- routes/users.js
+++ routes/users.js
@@ -12,11 +12,11 @@ module.exports = (db) => {
     res.send('respond with a resource');
   });
   
-  router.get('/:id', async (req, res) => {
-    const { id } = req.params
-    const { rows } = await db.query('SELECT * FROM users WHERE id = $1', [id])
-    res.end(rows[0])
-  })
+  // router.get('/:id', async (req, res) => {
+  //   const { id } = req.params
+  //   const { rows } = await db.query('SELECT * FROM users WHERE id = $1', [id])
+  //   res.end(rows[0])
+  // })
 
   return router;
 }
