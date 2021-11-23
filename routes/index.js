653dd2c4e2209b45c4078de31316e4e0ed5f1020
panoramine <camila.c.a.salles@gmail.com>
2021-11-23 09:30:32 -0800

    merging branch

--- routes/index.js
+++ routes/index.js
@@ -27,11 +27,10 @@ module.exports = (db) => {
   router.get('/users/:id', function(req, res) {
     const user_id = req.params.id;
 
-    
-
-
     res.render('user');
   });
+
+  
 
   return router;
 }
--- routes/users.js
+++ routes/users.js
@@ -4,19 +4,15 @@ const Router = require('express-promise-router');
 const router = new Router();
 
 module.exports = (db) => {
-  /* GET users listing. */
-  router.get('/', function(req, res, next) {
-    //db.query(`SELECT ....`)
-    //  .then(data => {
-    // })
-    res.send('respond with a resource');
+ 
+  router.get('/users/:id', function(req, res) {
+    const user_id = req.params.id;
+
+    
+
+
+    res.render('user');
   });
-  
-  // router.get('/:id', async (req, res) => {
-  //   const { id } = req.params
-  //   const { rows } = await db.query('SELECT * FROM users WHERE id = $1', [id])
-  //   res.end(rows[0])
-  // })
 
   return router;
 }
