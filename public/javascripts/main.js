30511fc0e1a4422d6629a62deba82c9d9b2f3c42
Colin Poon <me@colinpoon.com>
2021-11-22 16:49:46 -0800

    disabled "gamekeys" to fix keyboard lockout

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,38 +1,38 @@
-let toggle = false;
-let meltToggle = false;
+// let toggle = false;
+// let meltToggle = false;
 
-gsap.set('#level', { 
-    scaleY: 0,
-    transformOrigin: '50% 100%' 
-});
+// gsap.set('#level', { 
+//     scaleY: 0,
+//     transformOrigin: '50% 100%' 
+// });
 
-document.addEventListener('click', function(event) {
-    const target = event.target;
-    toggle = !toggle;
+// document.addEventListener('click', function(event) {
+//     const target = event.target;
+//     toggle = !toggle;
 
-    if (target.className === 'test' && toggle) {
-        gsap.to('h1', {duration: 2, opacity: 0.3, color:'white', backgroundColor: 'blue', rotation: 360, ease: 'bounce'});
-    }
+//     if (target.className === 'test' && toggle) {
+//         gsap.to('h1', {duration: 2, opacity: 0.3, color:'white', backgroundColor: 'blue', rotation: 360, ease: 'bounce'});
+//     }
 
-    if (target.className === 'test' && !toggle) {
-        gsap.to('h1', {duration: 2, opacity: 1.0, color:'black', backgroundColor: 'white', rotation: 0, ease: 'bounce'});
-    }
-});
+//     if (target.className === 'test' && !toggle) {
+//         gsap.to('h1', {duration: 2, opacity: 1.0, color:'black', backgroundColor: 'white', rotation: 0, ease: 'bounce'});
+//     }
+// });
 
-document.addEventListener('click', function(event) {
-    const target = event.target.closest('svg');
-    meltToggle = !meltToggle;
+// document.addEventListener('click', function(event) {
+//     const target = event.target.closest('svg');
+//     meltToggle = !meltToggle;
 
-    if (target.id === 'meltingpot' && meltToggle) {
-        gsap.to('#level', {duration: 3, scaleY: 1, ease: 'elastic(1, 0.8)'});
-        console.log('innnermeltclick');
-    }
+//     if (target.id === 'meltingpot' && meltToggle) {
+//         gsap.to('#level', {duration: 3, scaleY: 1, ease: 'elastic(1, 0.8)'});
+//         console.log('innnermeltclick');
+//     }
 
-    if (target.id === 'meltingpot' && !meltToggle) {
-        gsap.to('#level', {duration: 3, scaleY: 0, ease: 'power1.in'});
-    }
-    console.log(target);
-});
+//     if (target.id === 'meltingpot' && !meltToggle) {
+//         gsap.to('#level', {duration: 3, scaleY: 0, ease: 'power1.in'});
+//     }
+//     console.log(target);
+// });
 
 
 
