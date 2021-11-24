c5f31db8c38f54eaef5bd6e1e6a05cacdc3a702e
panoramine <camila.c.a.salles@gmail.com>
2021-11-23 20:10:51 -0800

    form data is now saved to database

---
+++ db/schema/02_datasets.sql
@@ -0,0 +1,5 @@
+CREATE TABLE datasets (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  dataset_name VARCHAR(255)
+);
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
   let revenueCounter = 1;
-  let expanseCounter = 1;
-  let expansesArray = [];
-  let revenuesArray = [];
+  let expenseCounter = 1;
+  let expensesArray = ["text-expense0", "num-expense0"];
+  let revenuesArray = ["text-revenue0", "num-revenue0"];
 
 
   const createRevenueInputElements = function (t_id, n_id) {
@@ -17,34 +17,26 @@ $(document).ready(function () {
   };
 
 
-  const createExpanseInputElements = function (t_id, n_id) {
-    const newExpanseInputs = (`
+  const createExpenseInputElements = function (t_id, n_id) {
+    const newExpenseInputs = (`
       <div class="title/value">
-        <input type="text" placeholder="expanse type" name='${t_id}' id='${t_id}'>
+        <input type="text" placeholder="expense type" name='${t_id}' id='${t_id}'>
         <input type="number" placeholder="value" name='${n_id}' id='${n_id}'>
       </div>
     `)
 
-    return newExpanseInputs;
+    return newExpenseInputs;
   };
 
 
-  const appendExpanseInputElements = function (t_id, n_id) {
-    $(".expanses").append(createExpanseInputElements(t_id, n_id))
+  const appendExpenseInputElements = function (t_id, n_id) {
+    $(".expenses").append(createExpenseInputElements(t_id, n_id))
   }
 
 
   const appendRevenueInputElements = function (t_id, n_id) {
     $(".revenues").append(createRevenueInputElements(t_id, n_id));
   };
-
-
-  $('.user-form').submit(function (event) {
-    event.preventDefault();
-
-    console.log("prevent default?")
-
-  })
 
 
   $(".add-revenue").on("click", function (e) {
@@ -59,120 +51,52 @@ $(document).ready(function () {
 
   });
 
-  $(".add-expanse").on("click", function (e) {
-    const idText = "text-expanse" + expanseCounter;
-    const idNum = "num-expanse" + expanseCounter;
+  $(".add-expense").on("click", function (e) {
+    const idText = "text-expense" + expenseCounter;
+    const idNum = "num-expense" + expenseCounter;
 
-    appendExpanseInputElements(idText, idNum);
+    appendExpenseInputElements(idText, idNum);
 
-    expansesArray.push(idText, idNum);
+    expensesArray.push(idText, idNum);
 
-    expanseCounter++;
+    expenseCounter++;
   })
 
-  // const userForm = document.querySelector('.user-form');
+  
+  $('.user-form').submit(function (event) {
+    event.preventDefault();
 
-  // userForm.addEventListener('click', addInputField);
+    let revenuesValues = [];
+    let expensesValues = []; 
 
-  // function addInputField(event) {
-  //   console.log("is it?")
-  //   const target = event.target;
-  //   if (target.className === 'add-revenue') {
-  //     const idText = "text-revenue" + revenueCounter;
-  //     const idNum = "num-revenue" + revenueCounter;
+    for (let i = 0; i < revenuesArray.length; i++) {
+      let tmpValueId = "#" + revenuesArray[i];
 
-  //     appendRevenueInputElements(idText, idNum)
+      revenuesValues.push($(`${tmpValueId}`).val());
+    }
 
-  //     revenuesArray.push(idText, idNum);
+    for (let i = 0; i < expensesArray.length; i++) {
+      let tmpValueId = "#" + expensesArray[i];
 
-  //     revenueCounter++;
-  //   }
+      expensesValues.push($(`${tmpValueId}`).val());
+    }
 
-  //   if (target.className === 'add-expanse') {
-  //     const idText = "text-expanse" + expanseCounter;
-  //     const idNum = "num-expanse" + expanseCounter;
+    const username = $('.username').text();
+    const datasetTitle = $('#dataset-title').val();
 
-  //     appendExpanseInputElements(idText, idNum)
+    $.ajax({
+      url: 'http://localhost:3000/users/datasets',
+      method: 'POST',
+      data: {
+        revenuesData: revenuesValues,
+        expensesData: expensesValues,
+        usernameData: username,
+        datasetTitle: datasetTitle
+      }
+    })
 
-  //     expansesArray.push(idText, idNum);
 
-  //     expanseCounter++;
-  //   }
-  // }
+  });
 
 });
 
-
-// $.ajax({
-//   url: '/api/movie-search',
-//   method: 'GET',
-//   data: inputData
-// }).then((result) => {
-//   return loadedMovies = result.map(movie => movie.title);
-
-// }).then((moviesArray) => {
-//   return moviesArray = moviesArray.map(movie => decoder(movie));
-
-// }).then((res) => {
-//   for (let movie of res) {
-//     let movieSuggestion = document.createElement("option");
-//     movieSuggestion.value = movie;
-//     suggestions.appendChild(movieSuggestion);
-
-//   }
-// })
-
-// $.ajax({
-//   type: 'POST',
-//   url: 'http://localhost:8080/api/search-result',
-//   data: $(this).serialize()
-// })
-// .then((res) => {
-//   let title = res[0]["title"];
-//   $('#movie-input').val("");
-//   appendMovie(title);
-
-//   // delete from selected
-//   const titleWithoutSpaces = title.replace(/\s+/g, '');
-//   $(`.${titleWithoutSpaces}`).on("click", () => {
-//     $(`#${titleWithoutSpaces}`).empty();
-//   })
-// })
-// })
-
-
-
-
-$.ajax({
-  type: 'POST',
-  url: 'http://localhost:8080/users/',
-  data: $(this).serialize()
-})
-  .then((res) => {
-    // I cant really test to see what this is doing cause i dont have control
-
-    // let amount = res[0]["revenue-value"];
-    $('#revenue-value').val("");
-    appendRevenueInputElements(amount)
-
-    const rmvHiddenChar = title.replace(/\s+/g, '');
-    $(`.${removeHiddenCharacters}`).on("click", () => {
-      $(`#${removeHiddenCharacters}`).empty();
-    })
-  })
-// const revenueData = $('#revenue').serialize();
-// $.ajax({
-//   type: 'POST',
-//   url: 'http://localhost:3080/users',
-//   data: revenueData,
-//   success: function (data) {
-//     console.log("SUCCESS WE DID THE AJAX CALL ON CLIENT'S END")
-//   },
-//   error: function (error) {
-//     console.log(error)
-//   }
-//   const titleWithoutSpaces = title.replace(/\s+/g, '');
-//    $(`.${titleWithoutSpaces}`).on("click", () => {
-//     $(`#${titleWithoutSpaces}`).empty();
-//   })
-// })
