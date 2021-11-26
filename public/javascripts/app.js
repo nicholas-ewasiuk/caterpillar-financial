3c6cce267e9a92f168dad57bdc0fc3f6c555f418
panoramine <camila.c.a.salles@gmail.com>
2021-11-25 16:50:46 -0800

    fix bug in app.js

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -158,16 +158,16 @@ $(document).ready(function () {
       for (let i = 0; i < revenuesArray.length; i++) {
 
         if (i % 2 === 0) {
-          $(`${revenuesArray[i]}`).val(`${result[0][i / 2].revenue_name}`);
-          $(`${revenuesArray[i + 1]}`).val(`${result[0][i / 2].amount}`);
+          $(`#${revenuesArray[i]}`).val(`${result[0][i / 2].revenue_name}`);
+          $(`#${revenuesArray[i + 1]}`).val(`${result[0][i / 2].amount}`);
         }
       }
 
       for (let i = 0; i < expensesArray.length; i++) {
 
         if (i % 2 === 0) {
-          $(`${expensesArray[i]}`).val(`${result[1][i / 2].expense_name}`);
-          $(`${expensesArray[i + 1]}`).val(`${result[1][i / 2].amount}`);
+          $(`#${expensesArray[i]}`).val(`${result[1][i / 2].expense_name}`);
+          $(`#${expensesArray[i + 1]}`).val(`${result[1][i / 2].amount}`);
         }
       }
 
