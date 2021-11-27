f6fdcebedc9c440b4d80c359ecb9e47fe215ebf2
Colin Poon <me@colinpoon.com>
2021-11-26 19:47:16 -0800

    small style changes

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,5 +1,6 @@
 INSERT INTO users (first_name, last_name, username, email, password)
-VALUES ('Colin', 'Poon', 'colinpoon', 'kiki@gmail.com', '123'),
+VALUES ('Boss', 'baby', 'BossBaby', 'alec@gmail.com', '123'),
+('Colin', 'Poon', 'colinpoon', 'kiki@gmail.com', '123'),
 ('Cindy', 'Ha', 'ch', 'cindy74@gmail.com', '123'),
 ('Kenny', 'Tse', 'kt', 'kenny_t@gmail.com', '123'),
 ('Cat', 'kitty', 'kitkat', 'kitkat@gmail.com', '123');
